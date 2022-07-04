d3bc9b0f9a2262e5fe3466fc9bcdc514d6d6c25f
murloctidecaller <102994544+murloctidecaller@users.noreply.github.com>
2022-07-04 21:45:24 +0300

    Initial commit

---
+++ src/main/java/FormDate.java
@@ -0,0 +1,7 @@
+public class FormDate {
+    //public static void birthday(String[] args) {
+        public String day;
+        public String month;
+        public String year;
+   // }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,1 @@
+public class Main {    public static void main(String[] args) {        Post post = new Post();        post.name = "Иван";        post.passport = "333 № 33333333";        post.patronymic = "Иванович";        post.phone = "+7 (777)-777-77-77";        post.surname = "Иванов";        post.subscription = true;    }}
